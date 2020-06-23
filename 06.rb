5c33ccfdd9ef7415a0cb3ba136da84f4d9f2c4d0
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-06-23 12:25:26 -0500

    Performing tests

--- 06.rb
+++ 06.rb
@@ -32,20 +32,20 @@ class GameLive
     tem_matrix = Array.new(10) { Array.new(10) { 0 } }
     (0...10).each do |i|
       (0...10).each do |j|
-        tem_matrix[i][j] = (@matrix[i][j]).zero ? juego_reglas(i, j, false) : juego_reglas(i, j, true)
+        tem_matrix[i][j] = (@matrix[i][j]) == 0 ? juego_reglas(i, j, false) : juego_reglas(i, j, true)
       end
     end
-    @matrix = temMatrix
+    @matrix = tem_matrix
+    return @matrix
   end
 
-  def juego_reglas(x, x, es_viva)
+  def juego_reglas(x, y, es_viva)
     @celulas_vivas = 0
     part1(x, y)
     part2(x, y)
     part3(x, y)
     part4(x, y)
-    retorno(es_viva)
-    return @resul
+    return retorno(es_viva)
   end
 
   def part1(x, y)
@@ -85,41 +85,45 @@ class GameLive
   end
 
   def retorno(es_viva)
-    @resul = if es_viva
-               (@celulas_vivas == 2 || @celulas_vivas == 3 ? 1 : 0)
-             else
-               (@celulas_vivas == 3 ? 1 : 0)
-             end
+    if es_viva
+      return (@celulas_vivas == 2 || @celulas_vivas == 3 ? 1 : 0)
+    else
+      return (@celulas_vivas == 3 ? 1 : 0)
+    end
   end
 end
 
-# require 'minitest/autorun'
-# class GamecaseTest < Minitest::Test
-#   def initialize; end
+# juego = GameLive.new
+# juego.llenar_game_random
+# juego.juego_dibujar
 
-#   def test_game_live
-#     testObject = GameLive.new
+require 'minitest/autorun'
+class GamecaseTest < Minitest::Test
+  def initialize; end
 
-#     testObject.llenadoManual(3, 1)
-#     testObject.llenadoManual(3, 2)
-#     testObject.llenadoManual(3, 3)
-#     testObject.juegoDibujar
+  def test_game_live
+    test_game = GameLive.new
 
-#     (0..0).each do |_i|
-#       res = testObject.jugandoGame
-#       puts
-#       testObject.juegoDibujar
-#     end
+    test_game.llenado_manual(3, 1)
+    test_game.llenado_manual(3, 2)
+    test_game.llenado_manual(3, 3)
+    test_game.juego_dibujar
 
-#     testMatrix = Array.new(10) { Array.new(10) { 0 } }
-#     testMatrix[2][2] = 1
-#     testMatrix[2][1] = 1
-#     testMatrix[2][3] = 1
+    res = ""
+    (0..3).each do |_i|
+      res = test_game.jugando_game
+      puts
+      test_game.juego_dibujar
+    end
 
-#     assert_equal res, testMatrix
-#     # assert_equal[]
-#   end
-# end
+    # testMatrix = Array.new(10) { Array.new(10) { 0 } }
+    # testMatrix[2][2] = 1
+    # testMatrix[2][1] = 1
+    # testMatrix[2][3] = 1
 
-# x = GamecaseTest.new
-# x.test_game_live
+    # assert_equal res, testMatrix
+  end
+end
+
+x = GamecaseTest.new
+x.test_game_live
